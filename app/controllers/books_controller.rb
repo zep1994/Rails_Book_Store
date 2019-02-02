19690c29ec0c617a62ebb5f6cec3a72a86ccd888
zep1994 <snaxbags@protonmail.com>
2019-02-01 20:45:03 -0600

    Finish CRUD

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,12 @@
 class BooksController < ApplicationController
+    before_action :book, only: [:show, :edit, :update, :destroy]
 
     def index 
-        @books = Book.all
+        @books = Book.all.order("created_at DESC")
     end 
 
     def new 
-        @books = Book.new
+        @book = Book.new
     end 
 
     def create 
@@ -17,16 +18,36 @@ class BooksController < ApplicationController
         end
     end
 
+    def edit 
+    end 
+
+    def update 
+        if @book.update 
+            redirect_to book_path(@book)
+        else 
+            render 'new'
+        end
+    end
+
     def show
     end
 
     def destroy
+        if @book.destroy 
+            redirect_to root_path 
+        else 
+            redirect_to book_path(@book)
+        end
     end 
 
     private 
 
     def book_params
-        require(:books).permit(:title, :description, :author)
+        params.require(:book).permit(:title, :description, :author)
+    end
+
+    def book 
+        @book = Book.find(params[:id])
     end
 
 
